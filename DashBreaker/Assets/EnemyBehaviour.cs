9c6fbf9fcd18c5281bbdf6a2d98a6eaff2e92b5d
XenoChrome000 <engdragod@gmail.com>
2024-02-26 15:11:43 +0800

    Enemy Tracking
    Simple enemy tracking.

---
+++ DashBreaker/Assets/EnemyBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public int distance;
+    public int moveSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeadToPlayer();
+    }
+
+    void HeadToPlayer()
+    {
+        if (Vector2.Distance(transform.position, player.position) >= distance)
+        {
+            Debug.Log("Checking");
+
+            transform.position = Vector3.Lerp(transform.position, player.position, moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, player.position) <= distance)
+            {
+                AttackPlayer();
+            }
+        }
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log("Attacking");
+    }
+}
