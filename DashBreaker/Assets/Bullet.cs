69970553082f18cb18afbfcc9ec0a2d8f06bab4d
XenoChrome000 <engdragod@gmail.com>
2024-02-28 20:03:57 +0800

    Added Bullet Deletion

--- DashBreaker/Assets/Bullet.cs
+++ DashBreaker/Assets/Bullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,23 +8,43 @@ public class Bullet : MonoBehaviour
     public Transform player;
     public Vector2 hold;
     public int moveSpeed;
+    public int distance;
+    public int expiry;
+    public bool check = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        hold = player.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (check == false)
+        {
+            hold = player.position;
+            check = true;
+        }
         FireAtPlayer();
     }
 
     void FireAtPlayer()
     {
         transform.position = Vector2.Lerp(transform.position, hold, moveSpeed * Time.deltaTime);
-        //Place on collision and destroy code here.
-        //Use gameobject.SetActive(false); instead of Destroy(gameObject)
+        if (Vector2.Distance(transform.position, player.position) <= distance)
+        {
+            gameObject.SetActive(false);
+            check = false;
+            //TakeDamage()
+        }
+        StartCoroutine(MissedBullet());
+    }
+
+    IEnumerator MissedBullet()
+    {
+        yield return new WaitForSeconds(expiry);
+        gameObject.SetActive(false);
+        check = false;
     }
 }
