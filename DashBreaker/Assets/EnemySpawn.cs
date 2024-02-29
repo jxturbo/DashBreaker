82a460cc963301fc530fb2f22c50bae32d22730f
XenoChrome000 <engdragod@gmail.com>
2024-02-29 20:21:54 +0800

    Random Delayed Spawning

--- DashBreaker/Assets/EnemySpawn.cs
+++ DashBreaker/Assets/EnemySpawn.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
     public int spawnNumber;
     public int randomNumber;
+    public int cooldown;
     public GameObject spawner;
     public List<GameObject> spawnList;
     public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("SpawnEnemy", 2.0f);
+        Invoke("SpawnEnemy", 0f);
     }
 
     void SpawnEnemy()
@@ -23,14 +25,19 @@ public class EnemySpawn : MonoBehaviour
         {
             spawnList.Add(tmp);
         }
-        spawnNumber = spawnList.Count;
+        StartCoroutine(RandomSpawn());
+    }
+    IEnumerator RandomSpawn()
+    {
         for (int i = 0; i < spawnNumber; i++)
         {
+            cooldown = Random.Range(0, 3);
             randomNumber = Random.Range(0, spawnList.Count);
             spawner = spawnList[randomNumber];
             Instantiate(spawner);
             Instantiate(enemy, spawner.transform.position, spawner.transform.rotation);
             enemy.SetActive(true);
+            yield return new WaitForSeconds(cooldown);
         }
     }
 }
