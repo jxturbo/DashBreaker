fd3b8e6a015db095b7faeb942270a68b054b85af
jxturbo <jxturbo@gmail.com>
2024-03-03 17:52:22 +0800

    extra setup

--- DashBreaker/Assets/Scripts/LevelUpAugment.cs
+++ DashBreaker/Assets/Scripts/LevelUpAugment.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelUpAugment : MonoBehaviour
 {
     public Modifiers[] modifiers;
+    public GameObject[] modifierCards;
     public PlayerController player;
     public float currentExpMultiplier;
 
