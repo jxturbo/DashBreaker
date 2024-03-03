662b69456c906d19cbe389badabdd930afbf2a29
jxturbo <jxturbo@gmail.com>
2024-03-03 17:49:30 +0800

    Level up modifier foundation

---
+++ DashBreaker/Assets/Scripts/LevelUpAugment.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpAugment : MonoBehaviour
+{
+    public Modifiers[] modifiers;
+    public PlayerController player;
+    public float currentExpMultiplier;
+
+    [System.Serializable]
+    public class Modifiers
+    {
+        public string attributeName;
+        public string description;
+        public float moveSpeedMultiplier = 1.1f;
+        public int damageAmountIncrease = 1;
+        public float maxDistanceMultiplier = 1.5f;
+        public float cooldownMultiplier = 1f;
+        public float expMultiplier;
+    }
+
+    void ApplyModifiers(Modifiers modifiers)
+    {
+        if (modifiers.moveSpeedMultiplier != 0f)
+            player.moveSpeed *= modifiers.moveSpeedMultiplier;
+
+        if (modifiers.damageAmountIncrease != 0)
+            player.damageAmount += modifiers.damageAmountIncrease;
+
+        if (modifiers.maxDistanceMultiplier != 0f)
+            player.maxDistance *= modifiers.maxDistanceMultiplier;
+
+        if (modifiers.cooldownMultiplier != 0f)
+            player.cooldown *= modifiers.cooldownMultiplier;
+
+        if (modifiers.expMultiplier != 0f)
+            currentExpMultiplier += modifiers.expMultiplier;
+    }
+
+    public void SelectModifier(int index)
+    {
+        if (index >= 0 && index < modifiers.Length)
+        {
+            ApplyModifiers(modifiers[index]);
+        }
+        else
+        {
+            Debug.LogError("Invalid index provided.");
+        }
+    }
+}
